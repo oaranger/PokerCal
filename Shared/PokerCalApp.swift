c148083bc7d5b95ae7b259640d34731f5d0cddff
Binh Huynh <bin.huy@gmail.com>
2022-12-03 17:33:29 -0800

    initial commit

---
+++ Shared/PokerCalApp.swift
@@ -0,0 +1,18 @@
+//
+//  PokerCalApp.swift
+//  Shared
+//
+//  Created by Binh Huynh on 12/3/22.
+//
+
+import SwiftUI
+
+@main
+struct PokerCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+asd
