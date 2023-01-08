6385913116660132f7380c00addb9592d94a4f11
Binh Huynh <bin.huy@gmail.com>
2023-01-08 14:57:43 -0800

    improve layout

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,14 +7,40 @@
 
 import SwiftUI
 
+extension String {
+    func fillSpace(limit: Int, alignment: Alignment = .trailing) -> Self {
+        if limit > count {
+            let arraySpace = Array(repeating: " ", count: limit - count).joined()
+            if alignment == .trailing {
+                return "\(arraySpace)" + self
+            } else {
+                return self + "\(arraySpace)"
+            }
+        }
+        return self
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func monoFontSpace() -> some View {
+        if #available(iOS 16, *) {
+            self
+                .monospaced()
+        } else {
+            self
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var players: [Player] = [
         Player(name: "Binh", buyInHistory: [50,100], checkout: 120, spent: 20),
         Player(name: "Hieu", buyInHistory: [100], checkout: 120),
-        Player(name: "Taii", checkout: 150, spent: 30),
+        Player(name: "Tai", checkout: 150, spent: 30),
         Player(name: "Hoang", buyInHistory: [50,50], checkout: 50, spent: 10),
-        Player(name: "Dooo", buyInHistory: [100, 100], checkout: 350),
-        Player(name: "Long", buyInHistory: [50,50], spent: 0)
+        Player(name: "Do", buyInHistory: [100, 100], checkout: 350),
+        Player(name: "Long", buyInHistory: [50,50], spent: 5)
     ]
     @State private var showingSheet = false
     @State private var selectedPlayer: Player = Player(name: "random")
@@ -59,33 +85,49 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 10) {
             List {
-                HStack(spacing: 6) {
+                HStack() {
                     Text("Name")
+                        .bold()
                         .frame(width: 70)
                     Text("Innn")
+                    Spacer()
                     Text("Outt")
+                    Spacer()
                     Text("Diff")
-                    Text("Adjust")
-                    Text("Food")
+                    Spacer()
+                    Text("Adju")
+                    Spacer()
+                    Text("Final")
                 }
-                .font(Font.body.bold())
+                .font(.system(size: 16, design: .monospaced))
                 .background(Color.yellow)
                 
                 ForEach(players, id: \.name) { player in
                     HStack {
-                        Text(player.name)
+                        Text(String(player.name).fillSpace(limit: 5, alignment: .leading))
                             .bold()
                             .frame(width: 70)
-                        Text("\(player.totalBuyIn)")
-                        Spacer()
-                        Text("\(player.checkout)")
-                        Spacer()
-                        Text("\(player.current)")
-                            .foregroundColor(player.winning ? Color.green : Color.red)
-                        Spacer()
-                        Text("\(player.misMatchAdjustment(groupWin: groupWin, groupLose: groupLose))")
-                        Spacer()
-                        Text("\(player.afterFood(groupSpent: groupSpent, groupWin: groupWin, groupLose: groupLose))")
+                            .font(.system(size: 36, design: .monospaced))
+                        
+                        Group {
+                            Text("\(player.totalBuyIn)".fillSpace(limit: 4))
+                            Spacer()
+                            Text("\(player.checkout)".fillSpace(limit: 4))
+                            Spacer()
+                            Text("\(player.current)".fillSpace(limit: 4))
+                                .foregroundColor(player.winning ? Color.green : Color.red)
+                            Spacer()
+                            Text("\(player.misMatchAdjustment(groupWin: groupWin, groupLose: groupLose))".fillSpace(limit: 4))
+                            Spacer()
+                        }
+                        .font(.system(size: 14, design: .monospaced))
+                        
+                            Text("\(player.afterFood(groupSpent: groupSpent, groupWin: groupWin, groupLose: groupLose))".fillSpace(limit: 5))
+                                .bold()
+                                .padding(.horizontal, 5)
+                                .background(player.afterFood(groupSpent: groupSpent, groupWin: groupWin, groupLose: groupLose) > 0 ? Color.green : Color.red)
+                                .cornerRadius(4)
+                                .fixedSize()
                     }
                     .contentShape(Rectangle())
                     .onTapGesture {
@@ -94,68 +136,95 @@ struct ContentView: View {
                     }
                 }
                 .onDelete(perform: deleteUser)
-            }
-            .border(isMismatched ? Color.red : Color.green, width: 2)
-            .padding(5)
-            
-            .frame(minHeight: UIScreen.main.bounds.size.height * 0.55)
-            .padding(.bottom, 60)
-            .addToBottom {
-                HStack {
-                    Button {
-                        isShowingResetAlert.toggle()
-                    } label: {
-                        Text("Reset")
+                
+                HStack() {
+                    HStack {
+                        Image(systemName: "fork.knife.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundColor(.orange)
+                        Text("Total = \(players.map { $0.spent }.reduce(0, +))")
                     }
-                    .alert("Are you sure to reset?", isPresented: $isShowingResetAlert) {
-                        Button {
-                            players = players.map { Player(name: $0.name) }
-                        } label: {
-                            Text("OK")
-                        }
-                        Button("Cancel", role: .cancel) {
-                        }
-                    }
-                    
-                    Spacer()
-                    
-                    Button {
-                        isAddingPlayer.toggle()
-                    } label: {
-                        Text("Add Player")
-                    }
-                }
-            }
-            
-            HStack {
-                Text("Win \(groupWin)").background(Color.green)
-                Text("Lose \(groupLose)").background(Color.red)
-                if isMismatched {
-                    if groupWin > abs(groupLose) {
-                        Text("Deficit \(groupWin - abs(groupLose))").bold().border(Color.red)
-                    } else {
-                        Text("Surplus \(abs(groupLose) - groupWin)").bold().border(Color.red)
-                    }
-                }
-            }
-            
-            VStack(alignment: .leading, spacing: 0) {
-                Text("Food/Drink")
-                List {
-                    ForEach(players, id:\.name) { player in
-                        if player.spent > 0 {
-                            HStack {
-                                Text("\(player.name)")
-                                Spacer()
-                                Text("\(player.spent)")
+                    .frame(maxWidth: .infinity)
+                    Divider()
+                    VStack(alignment: .leading) {
+                        ForEach(players, id:\.name) { player in
+                            if player.spent > 0 {
+                                Text("\(player.name) \(player.spent)")
                             }
                         }
                     }
+                    .frame(maxWidth: .infinity)
+                }
+                .padding(2)
+                
+                HStack {
+                    Spacer()
+                    Text("Win \(groupWin)")
+                        .modifier(TextDecor(color: .green))
+
+                    Text("Lose \(groupLose)")
+                        .modifier(TextDecor(color: .pink))
+                    
+                    if isMismatched {
+                        if groupWin > abs(groupLose) {
+                            Text("Deficit \(groupWin - abs(groupLose))")
+                                .bold()
+                                .modifier(TextDecor(color: .red))
+                        } else {
+                            Text("Surplus \(abs(groupLose) - groupWin)")
+                                .bold()
+                                .modifier(TextDecor(color: .red))
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.bottom, 10)
+            }
+            .listStyle(PlainListStyle())
+            .border(isMismatched ? Color.red : Color.green, width: 2)
+            .frame(minHeight: UIScreen.main.bounds.size.height * 0.55)
+            
+            HStack {
+                Button {
+                    isShowingResetAlert.toggle()
+                } label: {
+                    ZStack {
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 65, height: 65)
+                        Text("Reset")
+                            .bold()
+                            .foregroundColor(Color.white)
+                    }
+                }
+                .alert("Are you sure to reset?", isPresented: $isShowingResetAlert) {
+                    Button {
+                        players = players.map { Player(name: $0.name) }
+                    } label: {
+                        Text("OK")
+                    }
+                    Button("Cancel", role: .cancel) {
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    isAddingPlayer.toggle()
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
+                            .fill(Color.blue)
+                            .frame(width: 150, height: 50)
+                        Text("Add Player")
+                            .foregroundColor(Color.white)
+                    }
                 }
             }
-            .padding(2)
+            .padding(.vertical, 10)
+            .padding(.horizontal, 10)
+            
         }
-        
         .sheet(isPresented: $showingSheet) {
             print("Dismissing...")
             let index = players.firstIndex { player in
@@ -187,12 +256,17 @@ extension View {
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
 
-
-
-
+struct TextDecor: ViewModifier {
+  let color: Color
+  func body(content: Content) -> some View {
+    content
+      .padding(4)
+      .background(color.opacity(0.2))
+      .cornerRadius(8)
+      .foregroundColor(color)
+      .font(.title3)
+      .frame(maxWidth: .infinity)
+      .fixedSize()
+  }
+}
--- Shared/TextFieldAlert.swift
+++ Shared/TextFieldAlert.swift
@@ -32,53 +32,54 @@ struct TextFieldAlert<Presenting>: View where Presenting: View {
                         TextField("Spent", text: $spent)
                     }
                     .padding(.leading, 10)
-                    .frame(height: 36)
+                    .frame(height: 56)
                     .overlay(
                         RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 0.5)
                     )
                     
-                    
-                    Divider()
-                    
-                    HStack(alignment: .center) {
-                        Button(role: .cancel, action: {
-                            withAnimation {
-                                self.isShowing.toggle()
-                            }
-                        }) {
-                            Text("Cancel").padding(.horizontal)
-                        }
-                        
-                        Divider().padding(.horizontal)
-                        
+                    HStack {
                         Button(action: {
-                            withAnimation {
-                                var player = Player(name: name)
-                                player.buyInHistory = [(Int(buyIn) ?? 0, String.currentTime)]
-                                player.spent = Int(spent) ?? 0
-                                players.insert(player, at: 0)
-                                name = ""
-                                buyIn = ""
-                                spent = ""
-                                self.isShowing.toggle()
-                            }
+                            self.isShowing.toggle()
                         }) {
-                            Text("Add").padding(.horizontal)
+                            Text("Cancel")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .background(Color.red)
+                                .cornerRadius(5.0)
                         }
-                        .padding(.vertical)
+
+                        Button(action: {
+                            guard !name.isEmpty else { return }
+                            var player = Player(name: name)
+                            player.buyInHistory = [(Int(buyIn) ?? 0, String.currentTime)]
+                            player.spent = Int(spent) ?? 0
+                            players.insert(player, at: 0)
+                            name = ""
+                            buyIn = ""
+                            spent = ""
+                            self.isShowing.toggle()
+                        }) {
+                            Text("Add")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding()
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .background(Color.green)
+                                .cornerRadius(5.0)
+                        }
                     }
-                    .fixedSize(horizontal: false, vertical: true)
                 }
                 .padding()
                 .font(Font.body.bold())
-                .background(Color(#colorLiteral(red: 0.2025598161, green: 0.1036325959, blue: 0.5211219394, alpha: 1)))
                 .frame(
                     width: deviceSize.size.width*0.7,
-                    height: 250
+                    height: 375
                 )
                 .shadow(radius: 1)
                 .opacity(self.isShowing ? 1 : 0)
-                
+                .dimBG(condition: isShowing)
             }
         }
     }
@@ -94,3 +95,31 @@ extension View {
                        title: title)
     }
 }
+
+extension View {
+    @ViewBuilder
+    func dimBG(condition: Bool) -> some View {
+        if condition {
+            ZStack {
+                Color.black
+                    .opacity(0.5)
+                    .onTapGesture {
+                        hideKeyboard()
+                    }
+                self
+                    .background(Color.white)
+                    .cornerRadius(16)
+            }
+        } else {
+            self
+        }
+    }
+}
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
